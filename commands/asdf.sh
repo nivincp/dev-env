ce1192d362ff1f96bfc1dc93cce50154959f4350
Nivin <51740341+nivincp@users.noreply.github.com>
2026-01-15 23:03:41 +0700

    chore: update commands

--- commands/asdf.sh
+++ commands/asdf.sh
@@ -2,12 +2,15 @@ brew install asdf
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
 asdf install nodejs lts
 asdf install nodejs 24.0.1
 
 asdf install terraform 1.14.3
 asdf install terraform 1.9.8
+
+asdf install ruby 3.4.8
 
 asdf list all terraform
 asdf list all nodejs
